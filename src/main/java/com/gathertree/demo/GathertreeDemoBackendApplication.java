a1e4997ea39f1467e2a8eb058a14fdac46a0b9b7
sem_in_github <semin6634@naver.com>
2025-12-19 22:51:10 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/gathertree/demo/GathertreeDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gathertree.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GathertreeDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GathertreeDemoBackendApplication.class, args);
+	}
+
+}
