0d9caa7ef58e4ad7a17c6d59278e86f74abb1325
sem_in_github <semin6634@naver.com>
2025-12-21 12:18:10 +0900

    refactor: global 폴더 파일들 리펙토링

--- src/main/java/com/gathertree/demo/global/util/Base64ImageUtil.java
+++ src/main/java/com/gathertree/demo/global/util/Base64ImageUtil.java
@@ -1,12 +1,15 @@
 package com.gathertree.demo.global.util;
 
+import com.gathertree.demo.global.response.exception.GeneralException;
+import com.gathertree.demo.global.response.status.ErrorStatus;
+
 import java.util.Base64;
 
 public class Base64ImageUtil {
 
     public static byte[] decode(String base64) {
         if (!base64.contains(",")) {
-            throw new IllegalArgumentException("Invalid base64 image format");
+            throw new GeneralException(ErrorStatus.INVALID_IMAGE_FORMAT);
         }
         return Base64.getDecoder().decode(base64.split(",")[1]);
     }
